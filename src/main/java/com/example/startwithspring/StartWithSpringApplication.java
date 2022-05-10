d587ad0969896556c77bc61d19384551e44ab325
Shazeen Fabius <shazeen.fabius@measurabl.com>
2022-05-10 15:56:09 -0700

    student model

--- src/main/java/com/example/startwithspring/StartWithSpringApplication.java
+++ src/main/java/com/example/startwithspring/StartWithSpringApplication.java
@@ -1,13 +1,24 @@
 package com.example.startwithspring;
 
+import com.example.startwithspring.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class StartWithSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StartWithSpringApplication.class, args);
     }
 
+    @GetMapping
+    public List<Student> hello() {
+        return List.of(new Student(1L, "Shazeen", "shazeen@gmail.com", LocalDate.of(1993, 06, 15), 28));
+    }
 }
---
+++ src/main/java/com/example/startwithspring/student/Student.java
@@ -0,0 +1,27 @@
+package com.example.startwithspring.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+}
