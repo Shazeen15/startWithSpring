2e397c405930caa8cdcb41f02936241b252f2b94
Shazeen Fabius <shazeen.fabius@measurabl.com>
2022-05-10 15:39:06 -0700

    first commit

---
+++ src/test/java/com/example/startwithspring/StartWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
