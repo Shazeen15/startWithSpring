7a4b3ff42c8b01d51c678a71070751babc0af78b
Shazeen Fabius <shazeen.fabius@measurabl.com>
2022-05-13 12:45:13 -0700

    able to edit a student

--- src/main/java/com/example/startwithspring/student/StudentService.java
+++ src/main/java/com/example/startwithspring/student/StudentService.java
@@ -3,6 +3,7 @@ package com.example.startwithspring.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -35,5 +36,18 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional //the entity goes into a manage state
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exist"));
+
+        if (name != null) {
+            student.setName(name);
+        }
+
+        if (email != null) {
+            student.setEmail(email);
+        }
     }
 }
